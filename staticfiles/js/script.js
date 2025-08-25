c28079b8f43d8351d368f139ec3591f64604bcaa
Loja Online <davisouza.pro@hotmail.com>
2025-08-25 12:35:09 -0300

    Adicionado vídeo no app, centralizado e estilizado, sem controles e autoplay

---
+++ staticfiles/js/script.js
@@ -0,0 +1,23 @@
+function mostrarValor(produto, modelo, valor) {
+    alert(`Valor do ${produto} ${modelo}: R$${valor.toFixed(2)}`);
+}
+
+function IPHONE(modelo, valor) {
+    mostrarValor("iPhone", modelo, valor);
+}
+
+function XBOX(modelo, valor) {
+    mostrarValor("Xbox", modelo, valor);
+}
+
+function Playstation(modelo, valor) {
+    mostrarValor("Playstation", modelo, valor);
+}
+
+function teste() {
+    alert("funciona viu");
+}
+
+function VALOR() {
+    alert("R$=1500,00");
+}
