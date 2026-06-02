304f86b600e715774bf6770cde8eca79df154a45
Davi Souza <davimj99@gmail.com>
2026-06-02 20:44:23 -0300

    Migrando pra PostegreSQl e ataulizando algumas depedências

--- backend/settings.py
+++ backend/settings.py
@@ -1,15 +1,16 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = 'django-insecure-@tt%ep!d_^qungb-_xc6ykasny6l90v9k+ck%nv*d0*r(@t$d$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']  # Em produção, coloque os hosts corretos
+ALLOWED_HOSTS = []# Em produção, coloque os hosts corretos
 
 
 # Application definition
@@ -61,14 +62,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
